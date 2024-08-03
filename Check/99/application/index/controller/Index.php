b7301c45b4fd75d37003af94b565a2f0a4eb193f
SanHayes <126791399+SanHayes@users.noreply.github.com>
2024-08-03 14:14:46 +0700

    Update Index.php

--- Check/99/application/index/controller/Index.php
+++ Check/99/application/index/controller/Index.php
@@ -15,9 +15,9 @@ class Index extends Controller
         $HTTP_HOST_ARR = explode('.',$_SERVER['HTTP_HOST']);
         $HTTP_HOST = $HTTP_HOST_ARR[0] === 'www' ? $HTTP_HOST_ARR[1] . '.' . $HTTP_HOST_ARR[2] : $_SERVER['HTTP_HOST'];
         if(isMobile()){
-            $HOST = (isHttps() ? 'https://' : 'http://').'sj.'.$HTTP_HOST.'/99/index';
+            $HOST = (isHttps() ? 'https://' : 'http://').'sj.'.$HTTP_HOST.'/99';
         }else{
-            $HOST = (isHttps() ? 'https://' : 'http://').'dn.'.$HTTP_HOST.'/99/home';
+            $HOST = (isHttps() ? 'https://' : 'http://').'dn.'.$HTTP_HOST.'/99';
         }
         echo "<script language='javascript' type='text/javascript'>";  
         echo "window.location.href='$HOST'";  
